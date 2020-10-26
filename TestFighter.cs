eb27e25144b32f07747cfdae11f1ac54a45c4b9d
W8kon <W8kon@DESKTOP-NMQT8PU>
2020-10-25 23:03:09 -0400

    I should use commit and push to save

--- TestFighter.cs
+++ TestFighter.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class TestFighter : Fighter<TestFighter>
 {
+    // Changed code!!!
     public override void Awake()
     {
         base.Awake();
+        Debug.Log("We changed this thing");
     }
 
     public override void Update()
